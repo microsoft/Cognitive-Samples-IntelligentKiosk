8569fa54ccd267b1bd6233536ed5945aed7df2e4
Albert Davletov <albert.davletov@akvelon.com>
2020-10-08 17:33:59 -0700

    Update Settings page UX + move Mall Kiosk settings to the demo page

---
+++ Kiosk/Controls/NumericUpDown.xaml.cs
@@ -0,0 +1,92 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Cognitive Services: http://www.microsoft.com/cognitive
+// 
+// Microsoft Cognitive Services Github:
+// https://github.com/Microsoft/Cognitive
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace IntelligentKioskSample.Controls
+{
+    public sealed partial class NumericUpDown : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(int), typeof(NumericUpDown), new PropertyMetadata(default(int), OnValueChanged));
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register("Maximum", typeof(int), typeof(NumericUpDown), new PropertyMetadata(int.MaxValue));
+        public static readonly DependencyProperty MinimumProperty = DependencyProperty.Register("Minimum", typeof(int), typeof(NumericUpDown), new PropertyMetadata(default(int)));
+
+        public int Value
+        {
+            get { return (int)Convert.ToInt32(GetValue(ValueProperty)); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public int Maximum
+        {
+            get { return (int)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public int Minimum
+        {
+            get { return (int)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public NumericUpDown()
+        {
+            this.InitializeComponent();
+        }
+
+        private static void OnValueChanged(DependencyObject element, DependencyPropertyChangedEventArgs e)
+        {
+            //validate
+            var control = element as NumericUpDown;
+            if (control == null)
+            {
+                return;
+            }
+
+            //insure value is within range
+            control.Value = Math.Min(Math.Max(control.Value, control.Minimum), control.Maximum);
+        }
+
+        private void IncreaseValue(object sender, RoutedEventArgs e)
+        {
+            Value++;
+        }
+
+        private void DecreaseValue(object sender, RoutedEventArgs e)
+        {
+            Value--;
+        }
+    }
+}
