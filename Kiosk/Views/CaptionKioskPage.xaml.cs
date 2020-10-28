6b173dc236fc3055cf20c9e9438f768b61d3a317
Albert Davletov <albert.davletov@akvelon.com>
2020-10-28 16:11:39 -0700

    Add Caption Bot demo (#117)
    * Add Caption Bot demo
    
    * Add docs

---
+++ Kiosk/Views/CaptionKioskPage.xaml.cs
@@ -0,0 +1,193 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Cognitive Services: http://www.microsoft.com/cognitive
+// 
+// Microsoft Cognitive Services Github:
+// https://github.com/Microsoft/Cognitive
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using ServiceHelpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace IntelligentKioskSample.Views
+{
+    [KioskExperience(Id = "CaptionBot Kiosk",
+        DisplayName = "Caption Bot",
+        Description = "Get a description of the content of a webcam image",
+        ImagePath = "ms-appx:/Assets/DemoGallery/Caption Bot.jpg",
+        ExperienceType = ExperienceType.Automated | ExperienceType.Business,
+        TechnologiesUsed = TechnologyType.Vision,
+        TechnologyArea = TechnologyAreaType.Vision,
+        DateAdded = "2016/06/10")]
+    public sealed partial class CaptionKioskPage : Page
+    {
+        private Task processingLoopTask;
+        private bool isProcessingLoopInProgress;
+        private bool isProcessingPhoto;
+
+        public CaptionKioskPage()
+        {
+            this.InitializeComponent();
+
+            Window.Current.Activated += CurrentWindowActivationStateChanged;
+            this.cameraControl.FilterOutSmallFaces = true;
+            this.cameraControl.CameraAspectRatioChanged += CameraControl_CameraAspectRatioChanged;
+            this.cameraControl.ShowDialogOnApiErrors = SettingsHelper.Instance.ShowDialogOnApiErrors;
+            this.cameraControl.HideCameraControls();
+        }
+
+        private void CameraControl_CameraAspectRatioChanged(object sender, EventArgs e)
+        {
+            this.UpdateCameraHostSize();
+        }
+
+        private async void CurrentWindowActivationStateChanged(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            if ((e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.CodeActivated ||
+                e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.PointerActivated) &&
+                this.cameraControl.CameraStreamState == Windows.Media.Devices.CameraStreamState.Shutdown)
+            {
+                // When our Window loses focus due to user interaction Windows shuts it down, so we 
+                // detect here when the window regains focus and trigger a restart of the camera.
+                await this.cameraControl.StartStreamAsync(isForRealTimeProcessing: true);
+            }
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SettingsHelper.Instance.VisionApiKey))
+            {
+                await new MessageDialog("Missing Vision API Key. Please enter a key in the Settings page.", "Missing Vision API Key").ShowAsync();
+            }
+            else
+            {
+                await this.cameraControl.StartStreamAsync(isForRealTimeProcessing: true);
+                this.StartProcessingLoop();
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            this.isProcessingLoopInProgress = false;
+
+            Window.Current.Activated -= CurrentWindowActivationStateChanged;
+            this.cameraControl.CameraAspectRatioChanged -= CameraControl_CameraAspectRatioChanged;
+
+            await this.cameraControl.StopStreamAsync();
+            base.OnNavigatingFrom(e);
+        }
+
+        private void StartProcessingLoop()
+        {
+            this.isProcessingLoopInProgress = true;
+
+            if (this.processingLoopTask == null || this.processingLoopTask.Status != TaskStatus.Running)
+            {
+                this.processingLoopTask = Task.Run(() => this.ProcessingLoop());
+            }
+        }
+
+        private async void ProcessingLoop()
+        {
+            while (this.isProcessingLoopInProgress)
+            {
+                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                {
+                    if (!this.isProcessingPhoto)
+                    {
+                        this.isProcessingPhoto = true;
+                        if ((DateTime.Now - this.cameraControl.LastFaceTimestamp).TotalSeconds >= 60)
+                        {
+                            await ProcessCameraCapture(null);
+                        }
+                        else
+                        {
+                            await this.ProcessCameraCapture(await this.cameraControl.TakeAutoCapturePhoto());
+                        }
+                    }
+                });
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private async Task ProcessCameraCapture(ImageAnalyzer e)
+        {
+            if (e == null)
+            {
+                this.isProcessingPhoto = false;
+                this.cameraGuideText.Text = "I haven't seen a human face in a while...\r\nIs anybody there? Please wake me up!";
+
+                return;
+            }
+
+            DateTime start = DateTime.Now;
+
+            await Task.WhenAll(e.DescribeAsync(), e.DetectFacesAsync(detectFaceAttributes: true));
+
+            this.debugText.Text = string.Format("Latency: {0}ms", (int)(DateTime.Now - start).TotalMilliseconds);
+
+            string caption = e.AnalysisResult.Description?.Captions?.FirstOrDefault()?.Text;
+            if (!string.IsNullOrEmpty(caption))
+            {
+                if (!SettingsHelper.Instance.ShowAgeAndGender && Util.ContainsGenderRelatedKeyword(caption))
+                {
+                    this.cameraGuideText.Text = "Please enable Age/Gender prediction in the Settings Page to see the results";
+                }
+                else
+                {
+                    this.cameraGuideText.Text = string.Format("I think it's {0}", caption);
+                }
+            }
+            else
+            {
+                this.cameraGuideText.Text = "I am not sure what that is";
+            }
+
+            this.isProcessingPhoto = false;
+        }
+
+        private void OnPageSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.UpdateCameraHostSize();
+        }
+
+        private void UpdateCameraHostSize()
+        {
+            this.cameraHostGrid.Height = this.cameraHostGrid.ActualWidth / (this.cameraControl.CameraAspectRatio != 0 ? this.cameraControl.CameraAspectRatio : 1.777777777777);
+        }
+    }
+}
